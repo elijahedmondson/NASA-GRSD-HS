13f835407c6ead681d3a7c3364d31c7aaf007f92
elijahedmondson <elijah.edmondson@gmail.com>
2016-06-03 16:15:26 -0600

    file names

---
+++ 9. Biological Effect.R
@@ -0,0 +1,12 @@
+
+load("/Users/elijah/Desktop/R/QTL/WD/2. Binomial Mapping/Rdata/Total_Thyroid_QTL.Rdata")
+load(file = "~/Desktop/R/QTL/WD/GRSD.Rdata")
+
+get.effect.size(pheno, pheno.col = "Thyroid", chr = 2, qtl, probs, sdp.file = "~/Desktop/R/QTL/WD/HS_Sanger_SDPs.txt.bgz", markers)
+
+
+
+
+
+
+
